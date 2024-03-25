4836094f54a5bd7249d5599f83f1654b2c6430f8
VictorCodebase <victorkithinji@outlook.com>
2024-03-25 23:05:36 +0300

    confirmed question 1

--- Quiz_2/CheckCycleLinkedList/soln.py
+++ Quiz_2/CheckCycleLinkedList/soln.py
@@ -29,9 +29,7 @@ class Solution:
 
         
     def addToLinkiedList(self, head, val):
-        #The node for the new value I want to add
         newNode = Node(val)
-
         #check isthe head exists, if not, ill make a new node
         if not head:
             head = newNode
@@ -43,10 +41,28 @@ class Solution:
         while current.next:
             current = current.next
         
-        #after getting to the end, now add the new node
         current.next = newNode
+        return head
+    
+    def createCycleLinkedList(self, values, pos):
+        if not values:
+            return None
         
-        #return the head. it'll be a reference to the whole list
+        # Create the linked list without a cycle
+        head = Node(values[0])
+        current = head
+        for val in values[1:]:
+            current.next = Node(val)
+            current = current.next
+        
+        # If pos is valid, create a cycle by finding the node at position pos
+        # and connecting it to the tail of the linked list
+        if pos < len(values):
+            cycle_node = head
+            for i in range(pos):
+                cycle_node = cycle_node.next
+            current.next = cycle_node
+        
         return head
 
 
@@ -54,10 +70,12 @@ head = None
 
 soln: Solution = Solution()
 
-head = soln.addToLinkiedList(head, 30)
-head = soln.addToLinkiedList(head, 20)
-head = soln.addToLinkiedList(head, 10)
+# head = soln.addToLinkiedList(head, 30)
+# head = soln.addToLinkiedList(head, 20)
+# head = soln.addToLinkiedList(head, 10)
 
-soln.printLinkedList(head)
-print(soln.hasCycle(head))
+head = soln.createCycleLinkedList([1, 2, 3, 4, 5], 3)
+
+#soln.printLinkedList(head)
+print(soln.hasCycle(head)) #Prints out true
 
